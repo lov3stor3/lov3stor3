230d24adb462f43da95fa606ceacecd2ec4beb22
lov3stor3 <lov3stor3@gmail.com>
2019-09-12 22:34:44 +0300

    12/09 - fkin host

--- saleor/settings.py
+++ saleor/settings.py
@@ -417,7 +417,7 @@ bootstrap4 = {
 
 TEST_RUNNER = "tests.runner.PytestTestRunner"
 
-ALLOWED_HOSTS = get_list(os.environ.get("ALLOWED_HOSTS", 'lovestore.ro'))
+ALLOWED_HOSTS = get_list(os.environ.get('lovestore.ro'))
 ALLOWED_GRAPHQL_ORIGINS = os.environ.get("ALLOWED_GRAPHQL_ORIGINS", "*")
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
