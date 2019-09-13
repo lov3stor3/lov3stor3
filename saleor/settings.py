6ccd93781a510d80ad8ead651dcb08734427b543
lov3stor3 <lov3stor3@gmail.com>
2019-09-13 18:23:19 +0300

    13/09 trying2

--- saleor/settings.py
+++ saleor/settings.py
@@ -376,7 +376,7 @@ bootstrap4 = {
 
 TEST_RUNNER = "tests.runner.PytestTestRunner"
 
-ALLOWED_HOSTS = get_list(os.environ.get("ALLOWED_HOSTS",'http://lovestore.ro','lovestore.ro'))
+ALLOWED_HOSTS = get_list(os.environ.get("ALLOWED_HOSTS",'http://lovestore.ro'))
 ALLOWED_GRAPHQL_ORIGINS = os.environ.get("ALLOWED_GRAPHQL_ORIGINS", "*")
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
